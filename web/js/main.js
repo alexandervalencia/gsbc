842d6a5e3ae4b4e9bdb0078a538380f31cc79e32
Alex Valencia <alex@Alexs-MacBook-Pro.local>
2017-05-25 22:06:25 -0700

    Book test

--- web/js/main.js
+++ web/js/main.js
@@ -6,52 +6,50 @@ var REGEX_MON = /\w{3}/g;
 var REGEX_YEAR = /\d{4}/g;
 
 function newMember() {
-	auth.createUser(
-		{
+	auth.createUser({
 			email: user.email.value,
 			firstName: user.firstName.value,
 			lastName: user.lastName.value,
 			password: user.password.value
-		}
-	)
-	.then(
-		function() {
-			alert('Account sucessfully created!');
-			signIn();
-			user.reset();
-		}
-	)
-	.catch(
-		function() {
-			alert('Sign-up failed. Try again.');
-			user.reset();
-		}
-	);
+		})
+		.then(
+			function() {
+				alert('Account sucessfully created!');
+				signIn();
+				user.reset();
+			}
+		)
+		.catch(
+			function() {
+				alert('Sign-up failed. Try again.');
+				user.reset();
+			}
+		);
 }
 
 function signIn() {
 	auth.signInWithEmailAndPassword(user.email.value, user.password.value)
-	.then(function() {
-		alert('You are signed-in');
-		user.reset();
-		document.location.href = '/';
-	})
-	.catch(function(err) {
-		alert('Sign-in failed. Chcek your email/password and try again.');
-	});
+		.then(function() {
+			alert('You are signed-in');
+			user.reset();
+			document.location.href = '/';
+		})
+		.catch(function(err) {
+			alert('Sign-in failed. Chcek your email/password and try again.');
+		});
 }
 
 function signOut() {
 	auth.signOut()
-	.then(() => {
-		location.href = '/';
-	});
+		.then(() => {
+			location.href = '/';
+		});
 }
 
 var currentUser = auth.currentUser;
 
 if (currentUser) {
-	$('#greetMember').text('Hey ' + currentUser.firstName +'!');
+	$('#greetMember').text('Hey ' + currentUser.firstName + '!');
 	$('#signInButton').remove();
 	$('#footerNav').append('<a onclick="signOut;" return false;><button class="btn btn-primary" type="button">Sign Out</button></a> <button class="btn btn-primary" data-target="#addBookModal" data-toggle="modal" type="button">Add a Book</button>');
 } else {
@@ -69,22 +67,21 @@ form.addEventListener(
 			'author': form.author.value,
 			'book': form.title.value,
 			'datePicked': form.datePicked.value,
-			'submittedBy': currentUser,
 			'pickedBy': form.pickedBy.value
 		}
 
-	WeDeploy
-		.data('data.gsbc.wedeploy.io')
-		.create('books', values)
-		.then(
-			function(book) {
-				console.log(book);
-			}
-		)
-		.catch(
-			function(err) {
-				console.error(err);
-			}
-		);
+		WeDeploy
+			.data('data.gsbc.wedeploy.io')
+			.create('books', values)
+			.then(
+				function(book) {
+					console.log(book);
+				}
+			)
+			.catch(
+				function(err) {
+					console.error(err);
+				}
+			);
 	}
 );
