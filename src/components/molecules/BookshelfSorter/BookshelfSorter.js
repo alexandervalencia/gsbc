110b3126aafd19c4236b3c37e993c7f41c46b76b
Alexander Valencia <alexander.valencia@liferay.com>
2018-09-11 16:38:04 -0700

    Removes BookSortUtil from BookShelfSorter (now in reducer.js)

--- src/components/molecules/BookshelfSorter/BookshelfSorter.js
+++ src/components/molecules/BookshelfSorter/BookshelfSorter.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import * as actionTypes from '../../../store/actions';
-
-import BookSortUtil from '../../../utils/BookSortUtil';
 import SortOptionsList from '../../../utils/sortingOptions';
 import './BookshelfSorter.css';
 
