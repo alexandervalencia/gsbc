1a25709ef431113cfa38f6a1be48ad635f219aee
Alex Valencia <alexander.valencia@liferay.com>
2019-01-02 23:57:23 -0800

    Updates BookInfo to check if a subtitle exists

--- src/components/molecules/BookInfo/BookInfo.js
+++ src/components/molecules/BookInfo/BookInfo.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
-
+import { format } from 'date-fns';
 import './BookInfo.css';
 
 const BookInfo = ({ author, datePicked, subtitle, title }) => {
-  const date = moment(datePicked, 'x').format('MMMM YYYY');
+  const date = format(datePicked, 'MMMM YYYY');
 
   return (
     <div className="BookInfo">
-      <h1 className="title">{title}</h1>
-      <h3 className="subtitle">{subtitle}</h3>
+      {subtitle ? (
+        <h1 className="title">
+          {title}: <span className="subtitle">{subtitle}</span>
+        </h1>
+      ) : (
+        <h1 className="title">{title}</h1>
+      )}
       <h2 className="author">{author}</h2>
       <p className="date-read">{date}</p>
     </div>
