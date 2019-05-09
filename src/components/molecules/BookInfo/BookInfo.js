b02f5f2ca0a85171de2a04c4ce5e275b99881269
Alex Valencia <alexander.valencia@liferay.com>
2019-05-08 23:16:34 -0700

    Fix grid spacing issues

--- src/components/molecules/BookInfo/BookInfo.js
+++ src/components/molecules/BookInfo/BookInfo.js
@@ -7,16 +7,16 @@ const BookInfo = ({ author, datePicked, subtitle, title }) => {
   const date = format(datePicked, 'MMMM YYYY');
 
   return (
-    <>
+    <div className="BookInfo">
       <h1 className="title">{title}</h1>
-      <h2 className="subtitle">{subtitle ? `${subtitle}` : null}</h2>
+      {subtitle ? <h2 className="subtitle">{subtitle}</h2> : null}
       <h3 className="author">
         by <span className="author-name">{author}</span>
       </h3>
       <p className="date-read">
         read on <strong>{date}</strong>
       </p>
-    </>
+    </div>
   );
 };
 
